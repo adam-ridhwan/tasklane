452ea2853ce01dde84293d8a4258555760f3e726
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-03-08 09:07:25 -0500

    css

--- client/src/layouts/topBar/index.jsx
+++ client/src/layouts/topBar/index.jsx
@@ -1,11 +1,10 @@
-import { useEffect } from 'react';
 import AvatarPhotoImage from './components/avatarPhotoImage/avatarPhotoImage';
 import ExpandSideBarButton from './components/expandSideBarButton/expandSideBarButton';
 import GlobalActionsHeader from './components/globalActionsHeader/globalActionsHeader';
 import ShareButton from './components/shareButton/shareButton';
 import TabList from './components/tabList/tabList';
 
-import './topBar.styles.css';
+import './index.css';
 
 const TopBar = () => {
   return (
